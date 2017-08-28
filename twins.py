cbb13208114a9754dad9426ba5a22edec64a1d14
AdeleMortier <adele.mortier@gmail.com>
2017-08-28 16:08:46 +0200

    solution to twins

---
+++ twins.py
@@ -0,0 +1,22 @@
+def twins():
+    n_coins = int(input())
+    coins = str(input()).split()
+    coins = sorted([int(i) for i in coins], reverse=True)
+    
+    total_sum = sum(coins)
+    print(total_sum/2)
+    my_sum = 0
+    my_coins = 0
+    while my_sum <= total_sum/2:
+        my_sum += coins[my_coins]
+        my_coins += 1
+    print(my_coins)
+    
+    
+if __name__ == '__main__':
+    twins()
+        
+    
+   
+    
+    
